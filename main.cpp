e93404a3d400ab1edf6734efc31a7bdbd854ffc4
yesmey <jesper.meyr@gmail.com>
2016-11-24 23:28:46 +0100

    And fix example...

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "string_splitter.h"
 
 int main()
 {
     std::string ptr = "this,is,a,string,separated,,,,,,by*stars*and,commas";
-    const std::vector<meyer::StringPiece> splitted = meyer::split<',', '*'>(ptr);
-    for (const auto& itr : splitted) {
+    std::vector<meyer::StringPiece> vec;
+
+    meyer::split<',', '*'>(vec, ptr);
+    for (const auto& itr : vec) {
         if (!itr.empty()) {
             std::cout.write(itr, itr.size());
             std::cout << '\n';
